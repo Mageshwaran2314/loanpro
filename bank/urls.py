54d9a3b1e2652b103af8ea048e1d5f1f1124dc3c
mageshwaran2314 <magesh.amtl@gmail.com>
2019-12-18 21:33:52 +0530

    admin login

--- bank/urls.py
+++ bank/urls.py
@@ -22,7 +22,7 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     # path('lo/', views.login, name='lo'),
     path('', include('details.urls')),
 ]
